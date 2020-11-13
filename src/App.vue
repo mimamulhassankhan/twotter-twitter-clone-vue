de3de9e8710bf46dcca15bee7ef3e38464b1ab7f
mimamulhassankhan <pappuhassan440@gmail.com>
2020-11-14 00:38:48 +0600

    13 Nov Flag

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-
+    <UserProfile/>
   </div>
 </template>
 
 <script>
+import UserProfile from './components/UserProfile';
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {UserProfile}
 }
 </script>
 
@@ -16,8 +18,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2C3E50;
+  min-height: 100vh;
+  background-color: #F3F5FA
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div class="user_profile">
+            <div class="user_profile_user_panel">
+                <h1 class="user_profile_name">@{{user.userName}}</h1>
+                <div class="user_profile_follower_count">
+                    <strong>Followers: </strong> {{followers}}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'UserProfile',
+        data() {
+            return{
+                followers : 0,
+                user : {
+                    id: 1,
+                    userName: 'imamul_hk',
+                    firstName: 'Imamul',
+                    lastName: 'Hassan',
+                    email: 'pappuhassan440@gmail.com',
+                    isAdmin: true
+                }
+            }
+        },
+        computed: {
+            getFullName() {
+                return `${this.user.firstName} ${this.user.lastName}`;
+            }
+        },
+        methods: {
+            followUser(){
+                this.followers++;
+            }
+        },
+        mounted(){
+            this.followUser();
+        }
+    }
+</script>
+
+<style>
+    .user_profile{
+        display: grid;
+        grid-template-columns: 1fr 3fr;
+        width: 90%;
+        padding: 50px 5%;
+    }
+
+    .user_profile_user_panel{
+        display: flex;
+        flex-direction: column;
+        margin-right: 50px;
+        padding: 20px;
+        background-color: white;
+        border-radius:  5px;
+        border: 1px solid #DEF3E8;
+    }
+
+    h1{
+        margin: 0;
+    }
+
+</style>
